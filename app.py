b90aa434fa44b4369710e494fda59df5f11ab44d
Zanotto-Enrico <887566@stud.unive.it>
2022-06-09 18:45:11 +0200

    aggiunta grafica gestione corsi e profilo

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+from distutils.log import info
 import logging
 from logging.handlers import RotatingFileHandler
-from pickle import FALSE
+from pickle import FALSE, TRUE
 from flask import Flask, request, render_template, redirect, url_for, session
 import jyserver.Flask as jsf
 
@@ -95,16 +96,18 @@ def listaCorsi ():
 	else:
 		return redirect(url_for("login"))
 
-#Carica la pagina con la lista dei corsi disponibili
+#Carica la pagina per la creazione di un nuovo corso (solo per prof)
 @app.route ('/creaCorso', methods=['GET'])
 def creaCorso ():
 	if "user" in session and session["isProfessor"] == True:
-		return Calendario.render(render_template("creaCorso.html"))
+		pagina = Calendario
+		pagina.reset()
+		return pagina.render(render_template("creaCorso.html"))
 	else:
 		return redirect(url_for("login"))
 
-#Carica la pagina con la lista dei corsi disponibili
-@app.route ('/gestisciCorsi', methods=['GET'])
+#Carica la pagina di gestione corsi (solo per prof)
+@app.route ('/gestisciCorsi', methods=['GET','POST'])
 def gestisciCorsi ():
 	if "user" in session and session["isProfessor"] == True:
 		return render_template("gestisciCorsi.html")
@@ -112,12 +115,12 @@ def gestisciCorsi ():
 		return redirect(url_for("login"))
 
 #Effettua il logout dalla sessione
-@app.route ('/logout', methods=['GET'])
+@app.route ('/logout', methods=['GET','POST'])
 def logout():
 	session.pop("user", None)
 	return redirect(url_for("login"))
 
-#Carica la pagina con la pagina con il calendario
+#Carica la pagina con la pagina con il calendario delle lezioni
 @app.route ('/calendario', methods=['GET'])
 def calendario ():
 	if "user" in session:
@@ -125,7 +128,7 @@ def calendario ():
 	else:
 		return redirect(url_for("login"))
 
-#Carica la pagina di benvenuto
+#Carica la pagina di informazioni riguardanti il corso selezionato
 @app.route ('/infoCorso', methods=['POST'])
 def infoCorso ():
 	if "user" in session:
@@ -158,7 +161,9 @@ def infoCorso ():
 					"modalita": "In Presenza",
 					"durata": "7 lezioni",
 					"iscrizioni": "aperte",
-					"posti": "80/110",
+					"postimin": "10",
+					"postimax": "110",
+					"iscritti": "80",
 					"prof": "Edsger Dijkstra",
 					"descrizione": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
 					"inizio": "15/05/2022"}
@@ -166,5 +171,105 @@ def infoCorso ():
 			return render_template("infoCorso.html", info=diz, isProfessor=session["isProfessor"], result=result, iscritto=iscritto)
 		else:
 			return render_template("listaCorsi.html", isProfessor=session["isProfessor"])
+	else:
+		return redirect(url_for("login"))
+
+#Carica la pagina di gestione di un preciso corso
+@app.route ('/gestisciCorso', methods=['POST'])
+def gestisciCorso ():
+	if "user" in session:
+		if request.form.get("idCorso") != None : session["idCorso"] = request.form.get("idCorso")
+
+		if session["idCorso"] != None:											# DIZIONARIO DA RIEMPIRE CON LE INFO DEL CORSO
+			diz = { "id":12,
+					"nome": "Organizzazione di eventi culturali L'arte ai giovani! Incontriamo l'arte russa",
+					"struttura": "Centro Studi sulle Arti della Russia",
+					"posti": "",
+					"modalita": "In Presenza",
+					"durata": "7 lezioni",
+					"iscrizioni": "aperte",
+					"postimin": "10",
+					"postimax": "110",
+					"iscritti": "80",
+					"prof": "Edsger Dijkstra",
+					"descrizione": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
+					"inizio": "15/05/2022"}
+			
+			return render_template("gestisciCorso.html", info=diz, isProfessor=session["isProfessor"])
+		else:
+			return render_template("gestisciCorsi.html", isProfessor=session["isProfessor"])
+	else:
+		return redirect(url_for("login"))
+
+
+#Elimina il corso e reinderizza sulla pagina di gestione corsi
+@app.route ('/eliminaCorso', methods=['POST'])
+def eliminaCorso ():
+	if "user" in session and session["idCorso"] != None:
+			
+		if True:											# NELL'I VA VERIFICATO SE L'UTENTE È IL PROF PROPRIETARIO DEL CORSO
+			tmp = 0 # CHIAMARE LA FUNZIONE PER CANCELLARE IL CORSO
+
+		return redirect(url_for("gestisciCorsi"))
+
+	else:
+		return redirect(url_for("login"))
+
+#Carica la pagina di gestione di un preciso corso
+@app.route ('/modCorso', methods=['POST'])
+def modificaCorso ():
+	if "user" in session:
+
+		if session["idCorso"] != None:											# DIZIONARIO DA RIEMPIRE CON LE INFO DEL CORSO
+			diz = { "id":12,
+					"nome": "Organizzazione di eventi culturali L'arte ai giovani! Incontriamo l'arte russa",
+					"struttura": "Centro Studi sulle Arti della Russia",
+					"posti": "",
+					"modalita": "In Presenza",
+					"durata": "7 lezioni",
+					"iscrizioni": "aperte",
+					"postimin": "10",
+					"postimax": "110",
+					"iscritti": "80",
+					"prof": "Edsger Dijkstra",
+					"descrizione": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
+					"inizio": "15/05/2022"}
+			
+			return render_template("modCorso.html", info=diz, isProfessor=session["isProfessor"])
+		else:
+			return redirect(url_for("gestisciCorsi"))
+	else:
+		return redirect(url_for("login"))
+
+#Carica la pagina con la lista degli iscritti al corso
+@app.route ('/listaIscritti', methods=['GET','POST'])
+def listaIscritti ():
+	if "user" in session:
+		if session["idCorso"] != None:
+			lista =[    ["Cre_98","Crescenzo","Giustino","giustino@gmail.com","10"], 
+						["antonina_sev","Antonina", "Severina","antonina@hotmail.com","0"], 
+						["greg_nal","Naldo", "Gregorio","greg@gmail.com","5"], 
+						["MariSab0077","Mariano","Sabino","marix@stud.unive.it","9"], 
+						["arianna_888","Enrichetta","Arianna", "ari@gmail.com","11"]]
+
+			return render_template("iscritti.html", isProfessor=session["isProfessor"], lista=lista)
+	else:
+		return redirect(url_for("login"))
+
+#Carica la pagina con le info del profilo
+@app.route ('/profilo', methods=['GET','POST'])
+def profilo ():
+	if "user" in session:
+	
+		diz = { "id":12,
+				"username": "OttoZan",
+				"nome": "Carlo",
+				"cognome": "Rossi",
+				"email": "carlo.rossi@gmail.com",
+				"nascita": "30/02/1990",
+				"account": "studente",
+				"corsi": "10"}
+
+		return render_template("profilo.html", isProfessor=session["isProfessor"], info=diz)
 	else:
 		return redirect(url_for("login"))
